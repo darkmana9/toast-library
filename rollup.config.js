e25d81b990f2bc23f4cd159564a3fb5d0316d848
DRON <aka.cc@mail.ru>
2021-09-01 18:05:37 +0300

    feat: add configs

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import alias from "@rollup/plugin-alias";
+import path from 'path';
+
+const root = path.resolve(__dirname);
+
+export default {
+    input: "src/index.js",
+    output: {
+        file: "dist/bundle.js",
+        format: "iife",
+        sourcemap: true,
+    },
+    plugins: [
+        alias(
+            {
+                resolve: ['*', '.js', '.jsx'],
+                entries: [{
+                    find: '@',
+                    replacement: path.resolve(root, './src'),
+                }]
+            }
+        ),
+        nodeResolve({
+            extensions: [".js"],
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            preventAssignment: true
+        }),
+        babel({
+            presets: ["@babel/preset-react"],
+            babelHelpers: "runtime",
+            exclude: '/node_modules/**',
+        }),
+        commonjs(),
+        serve({
+            open: true,
+            verbose: true,
+            contentBase: ["", "public"],
+            host: "localhost",
+            port: 3000,
+        }),
+        livereload({watch: "src"}),
+       ]
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+
+import { Toast } from "@/components/Toast";
+
+ReactDOM.render(
+    <Toast/>,
+    document.getElementById('root'),
+)
