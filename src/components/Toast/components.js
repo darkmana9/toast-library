43348adadc96cb557a1a714372ffd63662c54013
DRON <aka.cc@mail.ru>
2021-09-01 18:10:23 +0300

    feat: add toast component

---
+++ src/components/Toast/components.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const ToastWrapper = styled.div`
+    width: 10vw;
+    height: 6vh;
+    background-color: red;
+ `
---
+++ src/components/Toast/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ToastWrapper } from "@/components/Toast/components";
+
+export const Toast = ({position,
+                          type,
+                          time,
+                          title,
+                          description,
+                          indent,
+                          color,
+                          animation})  => {
+        return (
+            <ToastWrapper>
+
+            </ToastWrapper>
+        )
+}
