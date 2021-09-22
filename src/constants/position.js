21a84fb2f577ff5fc4b44727f5970054aa22000b
DRON <aka.cc@mail.ru>
2021-09-22 14:26:22 +0300

    feat: add positions constants

--- src/constants/position.js
+++ src/constants/position.js
@@ -1,2 +1,31 @@
-export const TOAST_HEIGHT = 100
-export const TOAST_GAP = 10
+export const RB = {
+    right: 0,
+    bottom: 0,
+    left: 'auto',
+    top: 'auto',
+    name: 'RB'
+}
+
+export const LB = {
+    right: 'auto',
+    bottom: 0,
+    left: 0,
+    top: 'auto',
+    name: 'LB'
+}
+
+export const RT = {
+    right: 0,
+    bottom: 'auto',
+    left: 'auto',
+    top: 0,
+    name: 'RT'
+}
+
+export const LT = {
+    right: 'auto',
+    bottom: 'auto',
+    left: 0,
+    top: 0,
+    name: 'LT'
+}
