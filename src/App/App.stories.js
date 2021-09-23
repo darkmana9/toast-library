2eb8a453ba823d62e6ed1218d4babd10449aa254
DRON <aka.cc@mail.ru>
2021-09-23 11:38:22 +0300

    feat: add storybook

---
+++ src/App/App.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { App } from "@/App/index";
+
+
+export default {
+    title: "TestComponent",
+    component: App
+};
+
+export const Template = (args) => <App  {...args}/>;
+
+
