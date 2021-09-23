5370c993c090709210e83583bb4765ea30972c49
DRON <aka.cc@mail.ru>
2021-09-23 11:42:23 +0300

    feat: add PropTypes

--- src/App/index.js
+++ src/App/index.js
@@ -3,36 +3,58 @@ import React, { useState } from 'react'
 import { toast } from "@/service/ToastClass";
 import { PortalToast } from "@/components/PortalToast";
 import { LB, LT, RB, RT } from "@/constants/position";
+import PropTypes from "prop-types";
 
 
-export const App = () => {
+export const App = ({title, type, animation, color, gap, position, time}) => {
 
     const [pos, setPos] = useState({})
 
-    const showLB = () => {
-        setPos(LB)
-        toast.setTitle('LB').setType('error').setAnimation('fade').showToast()
+    const showToast = () => {
+        switch (position){
+            case 'LT': {
+                setPos(LT)
+                break
+            }
+            case 'LB': {
+                setPos(LB)
+                break
+            }
+            case 'RB': {
+                setPos(RB)
+                break
+            }
+            case 'RT': {
+                setPos(RT)
+                break
+            }
+        }
+        toast.setTitle(title).setType(type).setAnimation(animation).setColor(color).setGap(gap).setTime(time).showToast()
     }
-    const showRB = () => {
-        setPos(RB)
-        toast.setTitle('RB').setType('info').setAnimation('translate').showToast()
-    }
-    const showLT = () => {
-        setPos(LT)
-        toast.setTitle('LT').setType('success').setAnimation('translate').showToast()
-    }
-    const showRT = () => {
-        setPos(RT)
-        toast.setTitle('RT').setType('warning').setAnimation('fade').showToast()
-    }
-
     return (
         <>
             <PortalToast position={pos}/>
-            <button onClick={showLB}>LB</button>
-            <button onClick={showRB}>RB</button>
-            <button onClick={showLT}>LT</button>
-            <button onClick={showRT}>RT</button>
+            <button onClick={showToast}>show toast</button>
+
         </>
     )
 }
+App.propTypes = {
+    title: PropTypes.string,
+    color: PropTypes.string,
+    time: PropTypes.number,
+    type: PropTypes.oneOf(['error', 'warning', 'success', 'info']),
+    animation: PropTypes.oneOf(['fade', 'translate']),
+    position: PropTypes.oneOf(['LB', 'LT', 'RT', 'RB']),
+    gap: PropTypes.number,
+};
+
+App.defaultProps = {
+    title: 'tittle',
+    type: 'error',
+    color: '#37E29A',
+    animation: 'translate',
+    position: 'RT',
+    gap: 20,
+    time: 5000,
+};
